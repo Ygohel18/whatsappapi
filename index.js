bc4eff90de784beac459044b941b1505089b9eed
Yash Gohel <yashgohel16@gmail.com>
2023-02-02 18:22:08 +0530

    docker

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const cookieParser = require("cookie-parser");
 require('dotenv').config();
 
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 6000
 
 const webhook = process.env.WEBHOOK.split(",");
 
